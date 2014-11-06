d536a11891c3923048a056b95932f81be83231f0
Jeremy Hooke <jez@stulk.com>
2014-11-06 17:14:42 +1100

    Update fetch usage string for clarity.

--- fetch/auto.py
+++ fetch/auto.py
@@ -473,7 +473,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) != 2:
         sys.stderr.writelines([
-            'Usage: {} <config.yaml>\n'.format(sys.argv[0])
+            'Usage: fetch-service <config.yaml>\n'
         ])
         sys.exit(1)
 

50c5f3d058a39bdc6c76ec1e0a51c4d8ab4434c5
Duncan Gray <duncan.gray@ga.gov.au>
2020-05-24 22:22:43 +1000

    remove a baaad test

--- test/test_shellfileprocessor.py
+++ test/test_shellfileprocessor.py
@@ -17,10 +17,3 @@ def test_shellfilepro_required_files_not_there():
     file_path = os.path.abspath(__file__)
     sfp = ShellFileProcessor(command=command, expect_file=file_path, required_files=required_files)
     sfp.process(file_path)
-
-def test_shellfilepro_required_files_not_there():
-    command = 'diff {base} {base}.xml > {base}.xml.diff.txt'
-    required_files = ('^(?P<base>.*hdf)', ['{base}', '{base}.xml'])
-    file_path = '/home/osboxes/test_data/test_fetch/data/BRDF/MCD43A1.006/2020.05.01/MCD43A1.A2020122.h29v12.006.2020131040947.hdf'
-    sfp = ShellFileProcessor(command=command, expect_file=file_path, required_files=required_files)
-    sfp.process(file_path)

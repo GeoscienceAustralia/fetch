dfd47a4761b2f775507a4dcd4ce69f218e56a502
Duncan Gray <duncan.gray@ga.gov.au>
2020-05-25 15:26:46 +1000

    add an assert to a test

--- test/test_shellfileprocessor.py
+++ test/test_shellfileprocessor.py
@@ -9,11 +9,13 @@ def test_shellfilepro_required_files_there():
     required_files = ('^(?P<base>.*test.+)\\.py$', ['{base}.py', '{base}.py'])
     file_path = os.path.abspath(__file__)
     sfp = ShellFileProcessor(command=command, expect_file=file_path, required_files=required_files)
-    sfp.process(file_path)
+    results = sfp.process(file_path)
 
 def test_shellfilepro_required_files_not_there():
     command = 'ls {base}.py'
     required_files = (r'^(?P<base>.*test.+)\.py$', ['{base}.py', '/this/is/not/here/please.py'])
     file_path = os.path.abspath(__file__)
     sfp = ShellFileProcessor(command=command, expect_file=file_path, required_files=required_files)
-    sfp.process(file_path)
+    results = sfp.process(file_path)
+    # A rather toothless assert given required_files_there would return the same result...
+    assert file_path == results

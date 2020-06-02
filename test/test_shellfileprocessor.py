8be13ab31226941f906a2554effc92fc8d7f7396
Duncan Gray <duncan.gray@ga.gov.au>
2020-06-02 15:45:57 +1000

    updating the test

--- test/test_shellfileprocessor.py
+++ test/test_shellfileprocessor.py
@@ -8,7 +8,8 @@ def test_shellfilepro_required_files_there():
     # required_files = (r'^(?P<base>.*test.+)\.py$',['{base}.py','{base}.py'])
     required_files = ('^(?P<base>.*test.+)\\.py$', ['{base}.py', '{base}.py'])
     file_path = os.path.abspath(__file__)
-    sfp = ShellFileProcessor(command=command, expect_file=file_path, input_files=required_files)
+    expect_file = '{base}.py'
+    sfp = ShellFileProcessor(command=command, expect_file=expect_file, input_files=required_files)
     results = sfp.process(file_path)
 
 def test_shellfilepro_required_files_not_there():
